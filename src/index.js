49a26a781a2465fea2e0ce5f1ed8b5349d97edca
Sergey Andreev <keeper3211@gmail.com>
2018-03-05 23:19:30 +0300

    All test passet, 30ms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+    for(let i = 0, j = 0; i < 9; i++) {
+        let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+        let tempArray = [];
+        matrix[i] = matrix[i].map((value, i, array) => value === j ? (value = array.filter(item => !array.includes(item)),
+            tempArray = matrix.map(row => row[i]),
+            value = value.filter(item => !tempArray.includes(item)),
+            value = value.length === 1 ? value[j] : value) : value);
+    }
+    return matrix;
 }
